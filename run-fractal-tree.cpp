851ec4284b5125c578eb9d1f5f8d206d14236513
Hung Tran <hung@ae.cs.uni-frankfurt.de>
2020-11-10 11:00:20 +0100

    Add initialized repository

---
+++ run-fractal-tree.cpp
@@ -0,0 +1,15 @@
+/*
+ * run-fractal-tree.cpp
+ *
+ * Copyright (C) 2020 Henri Froese
+ *                    Hung Tran     <hung@ae.cs.uni-frankfurt.de>
+ */
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!" << std::endl;
+
+    return 0;
+}
