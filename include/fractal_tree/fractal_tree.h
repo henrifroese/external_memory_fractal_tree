8a3df08ec2d1b8793309f253ca6da0c4cd613837
Henri Froese <henri.froese@yahoo.com>
2020-12-07 18:34:08 +0100

    First steps of design of node/leaf/tree.

--- include/fractal_tree/fractal_tree.h
+++ include/fractal_tree/fractal_tree.h
@@ -8,7 +8,9 @@
 #ifndef EXTERNAL_MEMORY_FRACTAL_TREE_FRACTAL_TREE_H
 #define EXTERNAL_MEMORY_FRACTAL_TREE_FRACTAL_TREE_H
 
+#include <foxxll/mng/typed_block.hpp>
 #include "node.h"
+#include <unordered_map>
 
 namespace stxxl {
 
@@ -16,17 +18,35 @@ namespace fractal_tree {
 
 template <typename KeyType,
           typename DataType,
-          typename KeyCompareWithMaxType,
-          unsigned RawNodeSize,
-          unsigned RawLeafSize,
+          unsigned RawBlockSize,
           typename AllocStrategy>
 class fractal_tree {
+    // Type declarations.
     using key_type = KeyType;
     using data_type = DataType;
-    using key_compare = KeyCompareWithMaxType;
     using value_type = std::pair<const key_type, data_type>;
-    using node_type = node<key_type, data_type, key_compare, RawNodeSize>;
 
+    using self_type = fractal_tree<KeyType, DataType, RawBlockSize, AllocStrategy>;
+    using bid_type = foxxll::BID<RawBlockSize>;
+
+    using node_type = node<KeyType, DataType, RawBlockSize>;
+    using leaf_type = leaf<KeyType, DataType, RawBlockSize>;
+
+    using node_block_type = typename node_type::block_type;
+    using leaf_block_type = typename leaf_type::block_type;
+
+private:
+    std::unordered_map<int, node_type> node_id_to_node;
+    std::unordered_map<int, leaf_type> node_id_to_leaf;
+
+    /*
+     * Next steps:
+     *  - cache like in pq implementation
+     *  - id counters in fractal tree for nodes and leafs
+     *  - idea: tree wants new node -> creates BID and ID and constructs node with those, registers node in hashmap -> writes to node -> writes to cache
+     *          (problem: need to read into a typed_block object, but only want to use element at 0. Should change pointer in node and leaf classes to
+     *                    correct typed_block and then in all functions only access the 0th element.)
+     */
 public:
     // TODO remove
     fractal_tree() = default;
@@ -54,11 +74,10 @@ public:
 template <typename KeyType,
           typename DataType,
           typename KeyCompareWithMaxType,
-          unsigned RawNodeSize,
-          unsigned RawLeafSize,
+          unsigned RawBlockSize,
           typename AllocStrategy
           >
-using ftree = fractal_tree::fractal_tree<KeyType, DataType, KeyCompareWithMaxType, RawNodeSize, RawLeafSize, AllocStrategy>;
+using ftree = fractal_tree::fractal_tree<KeyType, DataType, KeyCompareWithMaxType, RawBlockSize, AllocStrategy>;
 
 }
 
--- include/fractal_tree/node.h
+++ include/fractal_tree/node.h
@@ -8,25 +8,88 @@
 #ifndef EXTERNAL_MEMORY_FRACTAL_TREE_NODE_H
 #define EXTERNAL_MEMORY_FRACTAL_TREE_NODE_H
 
+
+#include <foxxll/mng/typed_block.hpp>
+
+
 namespace stxxl {
 
-// TODO
 namespace fractal_tree {
 
+
 template<typename KeyType,
-         typename DataType,
-         typename KeyCompareWithMaxType,
-         unsigned RawNodeSize>
-class node {
+     typename DataType,
+     unsigned RawBlockSize>
+class node final {
+public:
+    // Basic type declarations
     using key_type = KeyType;
     using data_type = DataType;
-    using key_compare = KeyCompareWithMaxType;
     using value_type = std::pair<const key_type, data_type>;
+    using self_type = node<KeyType, DataType, RawBlockSize>;
+    using bid_type = foxxll::BID<RawBlockSize>;
+
+    // Set up sizes and types for the blocks used to store inner nodes' data in external memory.
+    enum {
+        max_num_values_in_node = (int) sqrt((double) RawBlockSize / (sizeof(KeyType) + sizeof(DataType))),
+        max_num_buffer_items_in_node = (int) (
+                (RawBlockSize - ((sizeof(KeyType) + sizeof(DataType) + sizeof(int)) * max_num_values_in_node)) /
+                (sizeof(KeyType) + sizeof(DataType))
+        )
+    };
+    // This is how the data of the inner nodes will be stored in a block.
+    struct node_block {
+        std::array<value_type, max_num_values_in_node>       values;
+        std::array<int,        max_num_values_in_node+1>     nodeIDs;
+        std::array<value_type, max_num_buffer_items_in_node> buffer;
+    };
+    using block_type = node_block;
+
+
+private:
+    const int m_id;
+    const bid_type m_bid;
+    int m_num_buffer_items = 0;
+    int m_num_values = 0;
+    block_type* m_block = NULL;
 
 public:
-    // TODO remove
-    node() = default;
+    explicit node(int ID, bid_type BID) : m_id(ID), m_bid(BID) {};
 };
+
+
+template<typename KeyType,
+        typename DataType,
+        unsigned RawBlockSize>
+class leaf final {
+    // Type declarations
+    using key_type = KeyType;
+    using data_type = DataType;
+    using value_type = std::pair<const key_type, data_type>;
+    using self_type = leaf<KeyType, DataType, RawBlockSize>;
+    using bid_type = foxxll::BID<RawBlockSize>;
+
+    // Set up sizes and types for the blocks used to store inner nodes' data in external memory.
+    enum {
+        max_num_buffer_items_in_leaf = (int) (RawBlockSize / (sizeof(KeyType) + sizeof(DataType)))
+    };
+    // This is how the data of the leaves will be stored in a block.
+    struct leaf_block {
+        std::array<std::pair<KeyType,DataType>, max_num_buffer_items_in_leaf> buffer;
+    };
+    using block_type = leaf_block;
+
+
+private:
+    const int m_id;
+    const bid_type m_bid;
+    int m_num_buffer_items = 0;
+    block_type* m_block = NULL;
+
+public:
+    explicit leaf(int ID, bid_type BID) : m_id(ID), m_bid(BID) {};
+};
+
 
 }
 
